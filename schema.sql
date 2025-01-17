56f44d9ea20c8be513d7614104a7b5f159c3e59a
joseph_bartram8 <josephbartram@outlook.com>
2025-01-17 14:05:24 +0000

    insert scripts are back

--- schema.sql
+++ schema.sql
@@ -24,3 +24,16 @@ CREATE TABLE coffee_beans (
 
 --create example user
 INSERT INTO users (username, password_hash, first_name, last_name) VALUES ('john', 'abc', 'John', 'Doe');
+
+--create coffee bean profile
+INSERT INTO coffee_beans (name, origin, roast_level, image_url, price_per_kg, stock_quantity, description)
+VALUES ('Sunrise Valley', 'Brazil', 'light', 'coffee_pouch2.png', 25, 100, 'A rich blend with hints of caramel and chocolate.');
+
+INSERT INTO coffee_beans (name, origin, roast_level, image_url, price_per_kg, stock_quantity, description)
+VALUES ('Moutain Peaks', 'Colombia', 'dark', 'coffee_pouch1.png', 25, 100, 'Smooth, fruity notes of Colombia''s finest coffee.');
+
+INSERT INTO coffee_beans (name, origin, roast_level, image_url, price_per_kg, stock_quantity, description)
+VALUES ('Sunset Roast', 'Ethiopia', 'medium', 'coffee_pouch3.png', 25, 100, 'A bold, dark roast with smoky undertones.');
+
+INSERT INTO coffee_beans (name, origin, roast_level, image_url, price_per_kg, stock_quantity, description)
+VALUES ('Golden Bean Farm', 'Kenya', 'light', 'coffee_pouch4.png', 25, 100, 'Light roasted with hints of caramel');
